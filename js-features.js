2208aea65b3c419e757f68c102eebef3c7c398b8
kian-tamjidi04 <106326113+kian-tamjidi04@users.noreply.github.com>
2024-08-31 17:04:44 +0100

    Add comments

--- js-features.js
+++ js-features.js
@@ -7,13 +7,13 @@ function createGrid(n) {
         box.className = "box";
         const boxSize = GRID_SIZE / n;
         box.style = `width: ${boxSize}px; height: ${boxSize}px; background-color: white; border: 1px solid black; box-sizing: border-box;`;
-
         return box;
     };
 
     const gridContainer = document.querySelector(".container");
     gridContainer.style = `display: grid; grid-template-columns: repeat(${n}, 1fr); width: ${GRID_SIZE}px; height: ${GRID_SIZE}px;`;
 
+    // Populates grid
     for (let i = 0; i < n * n; i++) {
         gridContainer.appendChild(createBox());
     }
@@ -22,6 +22,8 @@ function createGrid(n) {
 createGrid(16);
 
 const boxes = document.querySelectorAll(".box");
+
+// Changes the colour of a box if the mouse hovers over it
 boxes.forEach(box => {
     box.addEventListener("mouseover", () => {
         box.style.backgroundColor = "grey";
@@ -32,21 +34,26 @@ const sizeBtn = document.querySelector(".sizeBtn");
 sizeBtn.addEventListener("click", () => {
     let size = prompt("Enter new size for grid: ");
     size = parseInt(size);
+
+    // Input validation, size must be non-negative and not exceed 100
     if (size < 1 || size > 100) {
         alert("Value must be between 1 and 100");
         return;
     }
 
     const container = document.querySelector(".container");
+    // Removes the current grid from the DOM
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
+
+    // Creates new grid
     createGrid(size);
 
     const boxes = document.querySelectorAll(".box");
     boxes.forEach(box => {
         box.addEventListener("mouseover", () => {
             box.style.backgroundColor = "grey";
+        });
     });
-});
 });
