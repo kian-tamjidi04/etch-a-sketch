c6134faf93ad997e48a36cbd5e5d19c4ab0c775b
kian-tamjidi04 <106326113+kian-tamjidi04@users.noreply.github.com>
2024-08-31 16:06:59 +0100

    Delete gaps between boxes, edit colour

--- js-features.js
+++ js-features.js
@@ -6,13 +6,13 @@
 function createGrid() {
     const createBox = () => {
         const box = document.createElement("div");
-        box.style = "width: 20px; height: 20px; background-color: blue; border-color: black; border-width: 5px; border-style: solid; padding: 20px";    
+        box.style = "width: 20px; height: 20px; background-color: gray; border-color: black; border-width: 5px; border-style: solid";    
         return box;
     };
 
     const createLine = () => {
         const line = document.createElement("div");
-        line.style = "display: flex; gap: 10px";
+        line.style = "display: flex";
     
         // Each line contains 16 boxes
         for (let i = 0; i < 16; i++) {
@@ -23,7 +23,7 @@ function createGrid() {
     }
 
     const gridContainer = document.querySelector(".container");
-    gridContainer.style = "display: flex; gap: 10px; flex-direction: column"
+    gridContainer.style = "display: flex; flex-direction: column"
     for (let i = 0; i < 16; i++) {
         gridContainer.appendChild(createLine());
     }
