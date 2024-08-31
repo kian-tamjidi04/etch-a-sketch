d5e4bc8b9af22495366f8d7cd2184c396fe38a98
kian-tamjidi04 <106326113+kian-tamjidi04@users.noreply.github.com>
2024-08-31 16:04:09 +0100

    Finish createGrid() function

--- js-features.js
+++ js-features.js
@@ -1,15 +1,32 @@
 // TODO: CREATE A FUNCTION THAT CREATES DIVS FOR THE
 // 16X16 GRID
+
+// error: only one single box being rendered, which means each line contains either
+// one box of boxes stacked on top of each other
 function createGrid() {
-    // ADD DIVS 16 AT A TIME
-    // MAYBE CREATE ANOTHER FUNCTION THAT CREATES LINES
-    const box = document.createElement("div");
-    box.style = "width: 50px; height: 50px";
+    const createBox = () => {
+        const box = document.createElement("div");
+        box.style = "width: 20px; height: 20px; background-color: blue; border-color: black; border-width: 5px; border-style: solid; padding: 20px";    
+        return box;
+    };
 
-    const line = document.createElement("div");
-    line.style = "display: flex";
+    const createLine = () => {
+        const line = document.createElement("div");
+        line.style = "display: flex; gap: 10px";
+    
+        // Each line contains 16 boxes
+        for (let i = 0; i < 16; i++) {
+            line.appendChild(createBox());
+        }
 
-    for (let i = 0; i < 4; i++) {
-        line.appendChild(box);
+        return line;
+    }
+
+    const gridContainer = document.querySelector(".container");
+    gridContainer.style = "display: flex; gap: 10px; flex-direction: column"
+    for (let i = 0; i < 16; i++) {
+        gridContainer.appendChild(createLine());
     }
 }
+
+createGrid();
